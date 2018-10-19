d601575d7e46a2bc0ef0c4d6272598cc7c640619
Johann Schröder <jschroeder89@email.com>
2018-10-19 14:11:50 +0200

    added build folder

---
+++ CC/build/CMakeFiles/mobrob.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/johann/github/mobrob/CC/src/mobrob.cpp" "/home/johann/github/mobrob/CC/build/CMakeFiles/mobrob.dir/src/mobrob.cpp.o"
+  "/home/johann/github/mobrob/CC/src/randomNavigation.cpp" "/home/johann/github/mobrob/CC/build/CMakeFiles/mobrob.dir/src/randomNavigation.cpp.o"
+  "/home/johann/github/mobrob/CC/src/requestHandler.cpp" "/home/johann/github/mobrob/CC/build/CMakeFiles/mobrob.dir/src/requestHandler.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CC/build/CMakeFiles/mobrob.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mobrob.dir/src/mobrob.cpp.o"
+  "CMakeFiles/mobrob.dir/src/randomNavigation.cpp.o"
+  "CMakeFiles/mobrob.dir/src/requestHandler.cpp.o"
+  "mobrob.pdb"
+  "mobrob"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mobrob.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
